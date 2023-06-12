9ac6f7e7eb6ac77d3f7eca31bbe5f511dffccb4c
DineshKumar22122001 <126236053+DineshKumar22122001@users.noreply.github.com>
2023-06-12 19:38:00 +0530

    finished

--- src/Age.js
+++ src/Age.js
@@ -3,13 +3,13 @@ import React from "react";
 function Age({ errorClass, errorMessage, change, validateAge, formData }) {
   return (
     <div>
-      <div className="d-flex col-md-5 position-relative">
-        <label className="text-nowrap  col-lg-5 text-center p-2 bg-primary text-white border border-info rounded">
+      <div className="d-flex position-relative mb-4">
+        <label className="text-nowrap col-lg-5 text-center p-2 bg-secondary text-white border border-info rounded w-25">
           Age
         </label>
 
         <input
-          className={`w-100 border border-${errorClass} rounded`}
+          className={`w-50 border border-${errorClass} rounded`}
           type="number"
           name="age"
           value={formData.age}
--- src/Select.js
+++ src/Select.js
@@ -3,12 +3,12 @@ import React from "react";
 const Select = ({ labelName, formData, change, optionList, value, name }) => {
   console.log(formData);
   return (
-    <div className="col-lg d-flex">
-      <label className="text-nowrap col-md-3 text-center p-2 bg-primary text-white border border-info rounded">
+    <div className=" d-flex mb-3 ">
+      <label className="text-nowrap col-md-3 text-center p-2 bg-primary text-white border border-info rounded w-25">
         {labelName}
       </label>
       <select
-        className="form-select "
+        className="form-select w-50"
         name={name}
         value={value}
         onChange={change}
