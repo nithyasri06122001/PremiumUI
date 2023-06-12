ba0d419f853ef24796064aea8489d0e7e399d89f
DineshKumar22122001 <126236053+DineshKumar22122001@users.noreply.github.com>
2023-06-12 12:21:37 +0530

    select

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import WomenCare from "./womenCare";
+import Select from "./Select";
 const initialFormData = {
   productCode: "",
   productName: "",
@@ -88,51 +89,8 @@ function Home() {
     <div className="shadow  bg-light bg-gradient m-md-5 border rounded d-block ">
       <div className="row m-3">
         <p className="col-md">Quick Quote</p>
-        <div className="col-lg d-flex">
-          <label className="text-nowrap col-md-3 text-center p-2 bg-primary text-white border border-info rounded">
-            Product
-          </label>
-          <select
-            className="form-select "
-            name="productCode"
-            value={formData.productCode}
-            onChange={handleChange}
-          >
-            <option value="" selected disabled hidden>
-              Select Product
-            </option>
-            <option type="number" value="1">
-              Women Care
-            </option>
-            <option type="number" value="2">
-              Star Comprehensive
-            </option>
-            <option type="number" value="3">
-              Senior Citizen Red Carpet
-            </option>
-            <option type="number" value="4">
-              Star Micro Rural and Farmers Care
-            </option>
-            <option type="number" value="5">
-              Star Hospital Cash
-            </option>
-          </select>
-        </div>
+        <Select labelName="Product" />
       </div>
-      {formData.productCode === "1" && (
-        <WomenCare
-          submit={handleSubmit}
-          change={handleChange}
-          formData={formData}
-          errorClass={errorClass}
-          errorMessage={errorMessage}
-          validateAge={validateAge}
-        />
-      )}
-      {/* {formData.productCode === "2" && <WomenCare />}
-      {formData.productCode === "3" && <WomenCare />}
-      {formData.productCode === "4" && <WomenCare />}
-      {formData.productCode === "5" && <WomenCare />} */}
     </div>
   );
 }
---
+++ src/Select.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Select = ({ labelName }) => {
+  return (
+    <div>
+      <label>{labelName}</label>
+    </div>
+  );
+};
+
+export default Select;
