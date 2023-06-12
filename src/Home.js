72c655650b8b3c2d121a8079f6a1005b4c638781
DineshKumar22122001 <126236053+DineshKumar22122001@users.noreply.github.com>
2023-06-12 10:56:25 +0530

    product

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,35 @@
 import React from "react";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="col-lg d-flex">
+        <label className="text-nowrap col-md-3 text-center p-2 bg-primary text-white border border-info rounded">
+          Product
+        </label>
+        <select className="form-select " name="productCode">
+          <option value="" selected disabled hidden>
+            Select Product
+          </option>
+          <option type="number" value="1">
+            Women Care
+          </option>
+          <option type="number" value="2">
+            Star Comprehensive
+          </option>
+          <option type="number" value="3">
+            Senior Citizen Red Carpet
+          </option>
+          <option type="number" value="4">
+            Star Micro Rural and Farmers Care
+          </option>
+          <option type="number" value="5">
+            Star Hospital Cash
+          </option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
