a884a8c79df4aa9ae30f1f82fb374ec260f0e9e3
DineshKumar22122001 <126236053+DineshKumar22122001@users.noreply.github.com>
2023-06-13 09:42:29 +0530

    css

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ function Button({ type }) {
   return (
     <div className="m-3 text-center">
       <button
-        className="btn btn-primary text-white pe-3 ps-3 background_label"
+        className="btn btn-secondary text-white pe-3 ps-3 background_label border-0"
         type={type}
       >
         Get Quote
