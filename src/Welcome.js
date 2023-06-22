d47508658eabe1c9ef3da21de9d71defa69e5d44
Nithyasri <nithyasri.rb@purpleslate.in>
2023-06-22 09:24:20 +0530

    height fix

--- src/Welcome.js
+++ src/Welcome.js
@@ -64,9 +64,9 @@ const Welcome = () => {
       <svg xmlns="http://www.w3.org/2000/svg" class="dotted-circle" width="352" height="352" overflow="visible">
   <circle cx="176" cy="176" r="174" fill="none" stroke="#fff" stroke-width="2" stroke-miterlimit="10" stroke-dasharray="12.921,11.9271"/>
 </svg>
+        <button onClick={handleChange} className='btn-1'>GET QUOTE</button>
     </div>
   </div>
-        <button onClick={handleChange} className='btn-1'>GET QUOTE</button>
         </div>
      </div>
         
